13359153f006059edaeb027e9cc90fcb544532c9
Lucas Moura <lmoura@thoughtworks.com>
2017-11-27 10:39:12 -0200

    #12 [@lucasdornelas] - Fix data base

--- src/main/resources/db/migration/V1_1__CreateSchemaAndTable.sql
+++ src/main/resources/db/migration/V1_1__CreateSchemaAndTable.sql
@@ -1,6 +1,4 @@
-CREATE SCHEMA "master_mind_game_schema";
-
-CREATE TABLE 'master_mind_game_schema'.'player' (
+CREATE TABLE master_mind_game_schema.player (
   id                  SERIAL PRIMARY KEY,
   name                VARCHAR
 );
