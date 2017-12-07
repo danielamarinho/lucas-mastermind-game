9093daf6178bf7b28a21780b48b21f983c164796
Lucas Moura <lmoura@thoughtworks.com>
2017-12-07 14:58:59 -0200

    #19 [~lucasdornelas] Aply sugestions of code review

--- src/main/java/com/mastermind/game/repositories/PlayerRepository.java
+++ src/main/java/com/mastermind/game/repositories/PlayerRepository.java
@@ -1,7 +1,10 @@
 package com.mastermind.game.repositories;
 
 import com.mastermind.game.models.repository.PlayerEntity;
+<<<<<<< HEAD
 import com.mastermind.game.models.repository.PlayerRegister;
+=======
+>>>>>>> #19 [~lucasdornelas] Aply sugestions of code review
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
