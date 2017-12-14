a412acacf06df1b894c064e5d1aa4b016576eca8
Lucas Moura <lmoura@thoughtworks.com>
2017-12-14 18:59:03 -0200

    #19 [~lucasdornelas] correting comflicts

--- src/main/java/com/mastermind/game/repositories/PlayerRepository.java
+++ src/main/java/com/mastermind/game/repositories/PlayerRepository.java
@@ -1,10 +1,6 @@
 package com.mastermind.game.repositories;
 
 import com.mastermind.game.models.repository.PlayerEntity;
-<<<<<<< HEAD
-import com.mastermind.game.models.repository.PlayerRegister;
-=======
->>>>>>> #19 [~lucasdornelas] Aply sugestions of code review
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
