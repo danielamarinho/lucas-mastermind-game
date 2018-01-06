4f624c1fc6916077b16c755986b4b006ac086520
Lucas Moura <lmoura@thoughtworks.com>
2018-01-06 13:29:58 -0200

    #11 [~lucasdornelas] - Creating component test

---
+++ src/test/groovy/component/GetCurretGamesTest.groovy
@@ -0,0 +1,49 @@
+package component
+
+import com.mastermind.game.GameApplication
+import com.mastermind.game.models.Game
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootContextLoader
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+import javax.transaction.Transactional
+
+@ContextConfiguration(classes = GameApplication.class,
+        loader = SpringBootContextLoader.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+class GetCurretGamesTest extends Specification{
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    def 'Get current games'() {
+        given:('I creted two news games')
+
+        def gameOne = new Game(null, 1L).toJson().toString()
+        def gameTwo = new Game(null, 2L).toJson().toString()
+
+        HttpHeaders headers = new HttpHeaders()
+        headers.setContentType(MediaType.APPLICATION_JSON)
+
+        HttpEntity<String> entityOne = new HttpEntity<String>(gameOne, headers)
+        HttpEntity<String> entityTwo = new HttpEntity<String>(gameTwo, headers)
+
+        restTemplate.postForEntity('/games', entityOne, String.class)
+        restTemplate.postForEntity('/games', entityTwo, String.class)
+
+        when:('I give get in endpoint games')
+        def response = restTemplate.getForEntity('/games', String.class)
+
+        then:('I show HttpStatus.OK')
+
+        response.statusCode == HttpStatus.OK
+    }
+}
